a7fa5c8c31d0a3627b64cae39ab12d8c56c165b8
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-09-28 15:28:29 +0300

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+    // function handleClick(event) {
+    //     if (event===focus) {
+    //         console.log('Good!')
+    //     }
+    //     if (event===blur) {
+    //         console.log('Hey! Eyes on me!')
+    //     }
+    // }
+        return (
+            <div>
+                <button onFocus={(e)=>console.log("Good!")} onBlur={(e)=>console.log('Hey! Eyes on me!')}>Eyes on me</button>
+                
+            </div>
+        );
+
+
+}
+
+
+export default EyesOnMe;
